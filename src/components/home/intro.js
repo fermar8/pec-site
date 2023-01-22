f750fd7ccce0f0a8d5906d97fd8c4a69ae788a6d
fermar8 <ferran.ms8@gmail.com>
2023-01-22 19:30:33 +0100

    responsive intro, services and cardServices until 480px breakpoint

--- src/components/home/intro.js
+++ src/components/home/intro.js
@@ -1,78 +1,93 @@
 import {
   Button,
   Box,
-  Center,
   Flex,
   Image,
   Heading,
   VStack,
+  useMediaQuery,
 } from "@chakra-ui/react";
 
 import { WavingHand } from "../../components/icons";
 
 export default function intro() {
+  const isMobile = useMediaQuery("(max-width: 480px)");
   return (
     <Flex justify="center" alignItems="center" bg="brand.beige">
       <VStack
-        mt="80px"
-        mb="80px"
-        w={[280, 280, 728, 952, 1128]}
-        h="586px"
+        mt={["20px", null, null, null, null, null]}
+        w={[320, null, null, null, null, null]}
+        h={[508, null, null, null, null, null]}
         align="stretch"
         spacing="0px"
       >
-        <Box h="82px">
-          <Heading as="h1" fontSize="21px" fontWeight="normal">
+        <Box h={[184, null, null, null, null, null]}>
+          <Heading
+            as="h1"
+            fontSize={[18, null, null, null, null, null]}
+            fontWeight="normal"
+            mb={["16px", null, null, null, null, null]}
+          >
             Pàgines web web web web web
           </Heading>
-          <Heading as="h2" fontSize="38px">
+          <Heading as="h2" fontSize={[30, null, null, null, null, null]}>
             Agencia Marketing Digital y Diseño Web Barcelona
           </Heading>
         </Box>
-        <Box h="280px" boxShadow="0px 4px 50px rgba(0, 0, 0, 0.1)">
+        <Box
+          h={[140, null, null, null, null, null]}
+          boxShadow="0px 4px 50px rgba(0, 0, 0, 0.1)"
+        >
           <Image
-            w="1128px"
-            h="280px"
+            w={[320, null, null, null, null, null]}
+            h={[140, null, null, null, null, null]}
             src="img/Placeholder1128x280.jpg"
             alt="home-image"
             borderRadius="9px 9px 0px 0px"
           ></Image>
         </Box>
-        <Center
+        <Box
+          h={[152, null, null, null, null, null]}
+          w={[320, null, null, null, null, null]}
+          display="flex"
+          flexDirection="column"
           align="center"
           bg="brand.white"
           borderRadius="0px 0px 9px 9px"
           boxShadow="0px 4px 50px rgba(0, 0, 0, 0.1)"
         >
-          <Box
-            h="200px"
-            w="608px"
-            display="flex"
-            p="24px 0px 24px 0px"
-            flexDirection="column"
+          <Heading
+            as="h3"
+            fontSize={[16, null, null, null, null, null]}
+            fontWeight="light"
+            mt={["24px", null, null, null, null, null]}
+            mb={["24px", null, null, null, null, null]}
           >
-            <Heading as="h3" fontSize="18px" fontWeight="light" pb="24px">
-              Dissenyem i desenvolupem pàgines webs ràpides, visualment
-              atractives i amb una bona presència en línia.
-            </Heading>
-            <Heading as="h4" fontSize="18px" fontWeight="bold" pb="24px">
+            Dissenyem i desenvolupem pàgines webs ràpides, visualment atractives
+            i amb una bona presència en línia.
+          </Heading>
+          {!isMobile ? (
+            <Heading as="h4" fontSize="18px" fontWeight="bold" mb="24px">
               Comença de zero o contacta'ns per a millorar la teva pàgina web.
             </Heading>
-            <Button
-              bg="brand.red"
-              color="brand.white"
-              w="212px"
-              h="40px"
-              fontSize="14px"
-              verticalAlign="text-bottom"
-              borderRadius="999"
-              rightIcon={<WavingHand />}
-              alignSelf="center"
-            >
-              Contacta amb nosaltres
-            </Button>
-          </Box>
-        </Center>
+          ) : (
+            <></>
+          )}
+          <Button
+            bg="brand.red"
+            color="brand.white"
+            mb={["32px", null, null, null, null, null]}
+            w={[204, null, null, null, null, null]}
+            h={[32, null, null, null, null, null]}
+            fontSize={[14, null, null, null, null, null]}
+            verticalAlign="text-bottom"
+            borderRadius="999px"
+            rightIcon={<WavingHand />}
+            alignSelf="center"
+          >
+            Contacta amb nosaltres
+          </Button>
+        </Box>
       </VStack>
     </Flex>
   );
--- src/components/home/services.js
+++ src/components/home/services.js
@@ -1,107 +1,209 @@
-import { Button, Box, Flex, Heading, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Box,
+  Flex,
+  Heading,
+  VStack,
+  useMediaQuery,
+} from "@chakra-ui/react";
 
 import { WavingHand } from "../../components/icons";
 import CardService from "./cardService";
 
 export default function services() {
-  return (
-    <Flex justify="center" alignItems="center" bg="brand.pureWhite">
-      <VStack
-        mt="64px"
-        mb="64px"
-        w={[280, 280, 728, 952, 1128]}
-        h="1100px"
-        align="stretch"
-      >
-        <Box
-          w="821px"
-          h="136px"
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-around"
-        >
-          <Heading
-            as="h5"
-            fontSize="32px"
-            fontWeight="semi-bold"
-            color="brand.red"
-          >
-            Serveis web per a petites i mitjanes empreses
-          </Heading>
-          <Heading as="h6" fontSize="22px" fontWeight="light">
-            Que vulguin començar de zero o millorar la seva pàgina web existent,
-            captar nous clients, conèixer als existents i millorar la conversió
-            de venda en pàgina.
-          </Heading>
-        </Box>
-        <Box
-          h="940px"
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-around"
+  const isMobile = useMediaQuery("(max-width: 480px)");
+  if (isMobile) {
+    return (
+      <Flex justify="center" alignItems="center" bg="brand.pureWhite">
+        <VStack
+          mt={["64px", null, null, null, null, null]}
+          mb={["64px", null, null, null, null, null]}
+          w={[320, null, null, null, null, null]}
+          h={[1456, null, null, null, null, null]}
+          align="stretch"
         >
           <Box
-            h="422px"
-            w="1128px"
-            mt="48px"
-            mb="48px"
-            display="flex"
-            justifyContent="space-between"
-          >
-            <CardService />
-            <CardService />
-            <CardService />
-          </Box>
-          <Box
-            h="374px"
-            w="1128px"
-            mb="48px"
-            display="flex"
-            justifyContent="space-between"
-          >
-            <CardService />
-            <CardService />
-            <CardService />
-          </Box>
-          <Box
-            h="144px"
-            w="1128px"
+            w={[320, null, null, null, null, null]}
+            h={[296, null, null, null, null, null]}
+            mb={["24px", null, null, null, null, null]}
             display="flex"
             flexDirection="column"
-            justifyContent="space-between"
-            alignItems="center"
           >
-            <Box
-              h="80px"
-              w="821px"
-              textAlign="center"
-              fontSize="22px"
+            <Heading
+              as="h5"
+              fontSize={["24px", null, null, null, null, null]}
+              lineHeight={["40px", null, null, null, null, null]}
+              fontWeight="semi-bold"
+              color="brand.red"
+              mb={["16px", null, null, null, null, null]}
+            >
+              Serveis web per a petites i mitjanes empreses
+            </Heading>
+            <Heading
+              as="h6"
+              fontSize={["18px", null, null, null, null, null]}
+              lineHeight={["40px", null, null, null, null, null]}
               fontWeight="light"
             >
-              <p>
-                {" "}
-                Que vulguin començar de zero o millorar la seva pàgina web
-                existent, captar nous clients, conèixer als existents i millorar
-                la conversió de venda en pàgina.
-              </p>
-            </Box>
-            <Box h="40px" w="212px">
-              <Button
-                bg="brand.red"
-                color="brand.white"
-                w="204px"
-                h="32px"
-                fontSize="14px"
-                verticalAlign="text-bottom"
-                borderRadius="999"
-                rightIcon={<WavingHand />}
+              Que vulguin començar de zero o millorar la seva pàgina web
+              existent, captar nous clients, conèixer als existents i millorar
+              la conversió de venda en pàgina.
+            </Heading>
+          </Box>
+          <Box
+            h={[1136, null, null, null, null, null]}
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-around"
+          >
+            <CardService />
+            <CardService />
+            <CardService />
+            <CardService />
+            <CardService />
+            <CardService />
+            
+            <Box
+              h={[224, null, null, null, null, null]}
+              w={[320, null, null, null, null, null]}
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Box
+                textAlign="center"
+                mb={["24px", null, null, null, null, null]}
+                fontSize={["16px", null, null, null, null, null]}
+                lineHeight={["40px", null, null, null, null, null]}
+                fontWeight="light"
               >
-                Contacta amb nosaltres
-              </Button>
+                <p>
+                  {" "}
+                  Que vulguin començar de zero o millorar la seva pàgina web
+                  existent, captar nous clients, conèixer als existents i
+                  millorar la conversió de venda en pàgina.
+                </p>
+              </Box>
+              <Box h="40px" w="212px">
+                <Button
+                  bg="brand.red"
+                  color="brand.white"
+                  w="204px"
+                  h="32px"
+                  fontSize="14px"
+                  verticalAlign="text-bottom"
+                  borderRadius="999"
+                  rightIcon={<WavingHand />}
+                >
+                  Contacta amb nosaltres
+                </Button>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </VStack>
-    </Flex>
-  );
+        </VStack>
+      </Flex>
+    );
+  } else {
+    return (
+      <Flex justify="center" alignItems="center" bg="brand.pureWhite">
+        <VStack
+          mt={["64px", null, null, null, null, null]}
+          mb={["64px", null, null, null, null, null]}
+          w={[320, null, null, null, null, null]}
+          h="1100px"
+          align="stretch"
+        >
+          <Box
+            w="821px"
+            h="136px"
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-around"
+          >
+            <Heading
+              as="h5"
+              fontSize="32px"
+              fontWeight="semi-bold"
+              color="brand.red"
+            >
+              Serveis web per a petites i mitjanes empreses
+            </Heading>
+            <Heading as="h6" fontSize="22px" fontWeight="light">
+              Que vulguin començar de zero o millorar la seva pàgina web
+              existent, captar nous clients, conèixer als existents i millorar
+              la conversió de venda en pàgina.
+            </Heading>
+          </Box>
+          <Box
+            h="940px"
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-around"
+          >
+            <Box
+              h="422px"
+              w="1128px"
+              mt="48px"
+              mb="48px"
+              display="flex"
+              justifyContent="space-between"
+            >
+              <CardService />
+              <CardService />
+              <CardService />
+            </Box>
+            <Box
+              h="374px"
+              w="1128px"
+              mb="48px"
+              display="flex"
+              justifyContent="space-between"
+            >
+              <CardService />
+              <CardService />
+              <CardService />
+            </Box>
+            <Box
+              h="144px"
+              w="1128px"
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Box
+                h="80px"
+                w="821px"
+                textAlign="center"
+                fontSize="22px"
+                fontWeight="light"
+              >
+                <p>
+                  {" "}
+                  Que vulguin començar de zero o millorar la seva pàgina web
+                  existent, captar nous clients, conèixer als existents i
+                  millorar la conversió de venda en pàgina.
+                </p>
+              </Box>
+              <Box h="40px" w="212px">
+                <Button
+                  bg="brand.red"
+                  color="brand.white"
+                  w="204px"
+                  h="32px"
+                  fontSize="14px"
+                  verticalAlign="text-bottom"
+                  borderRadius="999"
+                  rightIcon={<WavingHand />}
+                >
+                  Contacta amb nosaltres
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </VStack>
+      </Flex>
+    );
+  }
 }
