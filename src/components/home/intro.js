9452fb3fab63c7e5aee76612e2216917569a4a86
fermar8 <ferran.ms8@gmail.com>
2023-05-25 16:55:27 +0200

    intro homepage done

--- src/components/home/intro.js
+++ src/components/home/intro.js
@@ -5,93 +5,126 @@ import {
   Heading,
   VStack,
   useMediaQuery,
+  chakra,
 } from "@chakra-ui/react";
 
-import Image from 'next/image';
+import Image from "next/image";
 
 import ButtonContactWithUs from "components/general/buttons/buttonContactWithUs";
 
-
 export default function Intro(props) {
-  const { title, subtitle, text, cta, buttonText } = props;
   const [isMobile] = useMediaQuery("(max-width: 768px)");
-  return (
-    <Flex justify="center" alignItems="center" bg="brand.warmBackground">
-      <VStack
-        mt={["20px", "20px", "40px", "80px", "80px", null]}
-        mb={[null, "40px", "80px", "80px", "80px", null]}
-        w={[320, 440, 728, 952, 1128, null]}
-        h={[584, 651, 600, 624, 582, null]}
-        align="stretch"
-        spacing="0px"
-      >
-        <Box
-          h={[184, 169, "96px", 124, "82px", null]}
-          mb={["16px", "16px", "16px", "16px", "16px", null]}
-        >
-          <Heading
-            as="h2"
-            fontSize={["18px", "21px", "21px", "21px", "21px", null]}
-            fontWeight="normal"
-            mb={["16px", "16px", "16px", "16px", "16px", null]}
-          >
-            {title}
-          </Heading>
-          <Heading
-            as="h1"
-            fontSize={["30px", "36px", "30px", "38px", "38px", null]}
-            lineHeight={["133%", "120%", "120%", "120%", null, null]}
-          >
-            {subtitle}
-          </Heading>
-        </Box>
-        <Box
-          h={[140, 280, 280, 280, 280, null]}
-          boxShadow="0px 4px 50px rgba(0, 0, 0, 0.1)"
-          borderRadius="9px 9px 0px 0px"
-          position="relative"
-        >
-          <Image
-            layout="fill"
-            src={`${process.env.basePath}/img/home/home-intro.png`}
-            alt="home-image"
-          ></Image>
-        </Box>
-        <Box
-          h={[208, 186, 208, 208, 204, null]}
-          w={[320, 440, 728, 952, 1128, null]}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          align="center"
-          bg="brand.whiteBackground"
-          borderRadius="0px 0px 9px 9px"
-          boxShadow="0px 4px 50px rgba(0, 0, 0, 0.1)"
-        >
-          <Box w={[320, 440, 728, 808, 952, null]}>
-            <Text
-              fontSize={["16px", "16px", "18px", "18px", "18px", null]}
-              fontWeight="light"
-              mt={["24px", "24px", "24px", "24px", "24px", null]}
-              mb={["24px", "24px", "24px", "24px", "24px", null]}
-            >
-              {text}
-            </Text>
-            {!isMobile ? (
-              <Text
-                fontSize={[null, null, "18px", "18px", "18px", null]}
-                fontWeight="bold"
-                mb="24px"
-              >
-                {cta}
-              </Text>
-            ) : (
-              <></>
-            )}
-            <ButtonContactWithUs buttonText={buttonText} />
-          </Box>
-        </Box>
-      </VStack>
-    </Flex>
+  const [isTablet] = useMediaQuery(
+    "(min-width: 768px) and (max-width: 1280px)"
   );
+  const { title, subtitle, text, buttonText } = props;
+
+  if (isMobile || isTablet) {
+    return (
+      <>
+        <Flex
+          justify="center"
+          bg="brand.whiteBackground"
+          minHeight="540px"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <VStack
+            w={[320, null, 528, null, null, null]}
+            minHeight={[480, null, 480, null, null, null]}
+            align="stretch"
+            justifyContent="space-around"
+            spacing="0px"
+          >
+            <Box>
+              <Box display="flex" justifyContent="center" width="100%">
+                <Image
+                  alignSelf="center"
+                  width={154}
+                  height={154}
+                  src={`${process.env.basePath}/img/digital-workplace.png`}
+                  alt="Treballadors ON LINE Marketing Digital"
+                ></Image>
+              </Box>
+              <Heading
+                as="h2"
+                fontSize={["22px", null, null, null, null, null]}
+                fontWeight="bold"
+                color="brand.main"
+                lineHeight="1"
+                mb="8px"
+              >
+                {title}
+              </Heading>
+              <Heading
+                as="h1"
+                fontSize={["24px", null, null, null, null, null]}
+                fontWeight="bold"
+                color="brand.text"
+                lineHeight="1"
+              >
+                {subtitle}
+              </Heading>
+            </Box>
+            <Text>{text}</Text>
+            <ButtonContactWithUs buttonText={buttonText} />
+          </VStack>
+        </Flex>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Flex
+          justify="center"
+          bg="brand.whiteBackground"
+          minHeight="500px"
+          alignItems="center"
+          flexDirection="row"
+        >
+          <VStack
+            w={[320, null, 528, null, 520, null]}
+            minHeight={[480, null, 480, null, 320, null]}
+            align="stretch"
+            justifyContent="space-around"
+            spacing="0px"
+          >
+            <Box>
+              <Heading
+                as="h2"
+                fontSize={["22px", null, null, null, null, null]}
+                fontWeight="bold"
+                color="brand.main"
+                lineHeight="1"
+                mb="8px"
+              >
+                {title}
+              </Heading>
+              <Heading
+                as="h1"
+                fontSize={["24px", null, null, null, null, null]}
+                fontWeight="bold"
+                color="brand.text"
+                lineHeight="1"
+              >
+                {subtitle}
+              </Heading>
+            </Box>
+            <Text>{text}</Text>
+            <Box>
+              <ButtonContactWithUs buttonText={buttonText} />
+            </Box>
+          </VStack>
+          <Box marginLeft="140px">
+            <Image
+              width={256}
+              height={256}
+              src={`${process.env.basePath}/img/digital-workplace.png`}
+              alt="Treballadors ON LINE Marketing Digital"
+            ></Image>
+          </Box>
+        </Flex>
+      </>
+    );
+  }
 }
