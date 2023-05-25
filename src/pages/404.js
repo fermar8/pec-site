7c3a953091fac4ae5aa38ecd8f3dfdb1527f424b
fermar8 <ferran.ms8@gmail.com>
2023-05-25 13:21:56 +0200

    404 for mobile and tablet

---
+++ src/pages/404.js
@@ -0,0 +1,118 @@
+import {
+  Text,
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  VStack,
+  useMediaQuery,
+} from "@chakra-ui/react";
+
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../components/layout";
+
+export default function Custom404() {
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const [isTablet] = useMediaQuery(
+    "(min-width: 768px) and (max-width: 1280px)"
+  );
+
+  if (isMobile || isTablet) {
+    return (
+      <Layout>
+        <Head>
+          <title>ON LINE | Quiénes Somos</title>
+          <meta
+            name="description"
+            content="ON LINE es una Agencia de Marketing Digital basada en Barcelona que ofrece todo lo que necesitas: posicionamiento SEO, SEM, Desarrollo Web, Diseño Web"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <VStack minHeight="400px" justifyContent="center">
+          <Image
+            height="140px"
+            width="140px"
+            src={`${process.env.basePath}/img/404error.png`}
+            alt="error-image"
+          ></Image>
+          <Box mb="40px">
+            <Heading
+              as="h1"
+              fontSize={["24px", null, null, null, null, null]}
+              fontWeight="bold"
+              color="brand.red"
+              lineHeight="1"
+            >
+              Error
+            </Heading>
+            <Heading
+              as="h2"
+              fontSize={["36px", null, null, null, null, null]}
+              fontWeight="bold"
+              color="brand.black"
+              lineHeight="1"
+            >
+              404
+            </Heading>
+          </Box>
+          <Text maxWidth="320px" textAlign="center">
+            Oops! Parece que te has desviado de la ruta correcta.
+          </Text>
+        </VStack>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <Head>
+          <title>ON LINE | Quiénes Somos</title>
+          <meta
+            name="description"
+            content="ON LINE es una Agencia de Marketing Digital basada en Barcelona que ofrece todo lo que necesitas: posicionamiento SEO, SEM, Desarrollo Web, Diseño Web"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Flex justify="center">
+          <HStack
+            minHeight="400px"
+            width="720px"
+            justifyContent="space-around"
+            alignSelf="center"
+          >
+            <Box mb="40px">
+              <Heading
+                as="h1"
+                fontSize={["24px", null, null, null, null, null]}
+                fontWeight="bold"
+                color="brand.red"
+                lineHeight="1"
+              >
+                Error
+              </Heading>
+              <Heading
+                as="h2"
+                fontSize={["36px", null, null, null, null, null]}
+                fontWeight="bold"
+                color="brand.black"
+                lineHeight="1"
+              >
+                404
+              </Heading>
+
+              <Text maxWidth="320px" textAlign="center">
+                Oops! Parece que te has desviado de la ruta correcta.
+              </Text>
+            </Box>
+            <Image
+              height="140px"
+              width="140px"
+              src={`${process.env.basePath}/img/404error.png`}
+              alt="error-image"
+            ></Image>
+          </HStack>
+        </Flex>
+      </Layout>
+    );
+  }
+}
