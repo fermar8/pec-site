30b503223c186b341a1aa449089c303c80432d17
fermar8 <ferran.ms8@gmail.com>
2023-05-27 20:05:49 +0200

    remove if isMobile || tablet

--- src/components/home/intro.js
+++ src/components/home/intro.js
@@ -13,13 +13,10 @@ import Image from "next/image";
 import ButtonContactWithUs from "components/general/buttons/buttonContactWithUs";
 
 export default function Intro(props) {
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
-  const [isTablet] = useMediaQuery(
-    "(min-width: 768px) and (max-width: 1280px)"
-  );
+  const [isMobileOrTablet] = useMediaQuery("(max-width: 1280px)");
   const { title, subtitle, text, buttonText } = props;
 
-  if (isMobile || isTablet) {
+  if (isMobileOrTablet) {
     return (
       <Flex
         justify="center"
