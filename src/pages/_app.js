a11d99bf0712ad6859a27a29681864643536b43f
fermar8 <ferran.ms8@gmail.com>
2023-05-30 23:49:35 +0200

    rollback to theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,35 @@ const ChakraProvider = dynamic(() => import("@chakra-ui/provider").then(
         (mod) => mod.ChakraProvider), { ssr: false }
 );
 
-import extendedTheme from "../components/extendedTheme";
+import { extendTheme } from "@chakra-ui/react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ weight: "400", subsets: ["latin"] });
+
+const fonts = {
+  heading: inter.style.fontFamily,
+  body: inter.style.fontFamily,
+};
 
 function MyApp({ Component, pageProps = {} }) {
+  const theme = extendTheme({
+    colors: {
+      brand: {
+        main: "#F55951",
+        border: "#FFE2DC",
+        whiteBackground: "#ffffff",
+        warmBackground: "#fff9f4",
+        text: "#292929",
+        cardBackground: "#E5E5E5",
+        grey: "#808080",
+        buttonHover: "#CE352D",
+        buttonActive: "#930C05",
+      },
+    },
+    fonts,
+  });
   return (
-    <ChakraProvider theme={extendedTheme} cssVarsRoot="#__next">
+    <ChakraProvider theme={theme} cssVarsRoot="#__next">
       <Component {...pageProps} />
     </ChakraProvider>
   );
